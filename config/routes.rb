7e90aac7c54b491bc34a57f3adce5770dc8dc12c
Christopher Mack <mack.c@ghc.org>
2015-02-12 18:32:36 -0800

    Basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders, only: [:index, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'orders#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
